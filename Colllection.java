bbdb739b5a11257cb23168b05ab35655256e3e72
ayush9634 <128793268+ayush9634@users.noreply.github.com>
2023-09-25 14:20:15 +0530

    Add files via upload

---
+++ Colllection.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.colllection;
+
+/**
+ *
+ * @author E-LAB-PC28
+ */
+import java.util.*;
+public class Colllection {
+
+    public static void main(String[] args) {
+       ArrayList al = new ArrayList();
+       al.add(12);
+       al.add("Hello");
+       al.add(12.5f);
+       al.add(132.1736892);
+       al.add('$');
+        System.out.println("Elements: "+al );
+    }
+}
